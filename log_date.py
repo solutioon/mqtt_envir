6ec82019e1b40939969d8d18833983f448de8ae6
solutioon <41238228+solutioon@users.noreply.github.com>
2022-12-13 15:54:40 +0100

    Update log_date.py

--- log_date.py
+++ log_date.py
@@ -4,4 +4,7 @@ import os
 timestr = time.strftime("%Y%m%d")
 print(timestr[-6:])
 
-
+print(os.getenv("BROKER_URL"))
+print(os.getenv("BROKER_PORT"))
+print(os.getenv("BROKER_USER"))
+print(os.getenv("BROKER_PASSWORD"))
